1400a499b270f0a8a263cfe3b814ac0668501c6f
frindaddy <58454817+frindaddy@users.noreply.github.com>
2023-05-29 00:34:33 -0500

    Update TagColors.js

--- client/src/format/TagColors.js
+++ client/src/format/TagColors.js
@@ -1,6 +1,26 @@
 const TagColors = {
+<<<<<<< Updated upstream
     gin: "#cb177f",
     misc: "#4b0404"
+=======
+    gin: "#1d2a87",
+    vodka: "#e12729",
+    rum: "#b29652",
+    tequila: "#8dc832",
+    mezcal: "#409f9d",
+    whiskey: "#b26a30",
+    campari: "#b1cf20",
+    aperol: "#da3fc6",
+    spirit: "#8a6a67",
+    glass: "#ce63ec",
+    style: "#690ae1",
+    taste: "#ab4efa",
+    mix: "#022b0c",
+    color: "#c63d17",
+    season: "#180128",
+    temp: "#934752",
+    misc: "#5b3cda",
+>>>>>>> Stashed changes
 };
 
 export default TagColors;
