c34802a2d6f31a0d216d6ee5ce7226630ad77225
frindaddy <58454817+frindaddy@users.noreply.github.com>
2023-07-05 17:08:12 -0500

    alphabetized glass list

--- client/src/components/Admin/GlassTypes.js
+++ client/src/components/Admin/GlassTypes.js
@@ -5,14 +5,14 @@ const GlassTypes = [
     {name: 'flute', displayName: 'Champagne Flute'},
     {name: 'margarita', displayName: 'Margarita'},
     {name: 'martini', displayName: 'Martini'},
+    {name: 'mule', displayName: 'Mule'},
     {name: 'nickandnora', displayName: 'Nick & Nora'},
     {name: 'pilsner', displayName: 'Pilsner'},
     {name: 'pitcher', displayName: 'Pitcher'},
     {name: 'rocks', displayName: 'Rocks'},
     {name: 'shot', displayName: 'Shot'},
-    {name: 'wine', displayName: 'Wine'},
     {name: 'tiki', displayName: 'Tiki'},
-    {name: 'mule', displayName: 'Mule'},
+    {name: 'wine', displayName: 'Wine'},
 ];
 
 export default GlassTypes;
