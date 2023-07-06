9982794c9a41822439f5eb2226d2e1a3868b1b37
sidezbros <ts.sides@gmail.com>
2023-07-06 15:42:37 -0500

    Improve filter framework
    Prepares the drink array for filtering beyond search terms

--- client/src/components/DrinkList/DrinkArray.js
+++ client/src/components/DrinkList/DrinkArray.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import DrinkEntry from "./DrinkEntry";
 
 const DrinkArray = ({ filter, drinkList, adminKey, setCurrentPage, setCurrentDrink, getDrinkList }) => {
 
+    const [filteredList, setFilteredList] = useState(drinkList);
+
+    useEffect(() => {
+        setFilteredList(drinkList.filter((drink) => {
+            return drink.name.toLowerCase().includes(filter.text.toLowerCase())
+        }))
+    }, [drinkList, filter]);
+
     return (
         <div>
-            {drinkList.map((drink) => {
-                if (drink.name.toLowerCase().includes(filter.text.toLowerCase())){
-                    return <DrinkEntry drink={drink} setCurrentPage={setCurrentPage} setCurrentDrink={setCurrentDrink} getDrinkList={getDrinkList} adminKey={adminKey}/>
-                }
+            {filteredList.map((drink) => {
+                return <DrinkEntry drink={drink} setCurrentPage={setCurrentPage} setCurrentDrink={setCurrentDrink} getDrinkList={getDrinkList} adminKey={adminKey}/>
             })}
         </div>
     )
