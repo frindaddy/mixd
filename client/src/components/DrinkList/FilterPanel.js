bfefe64ad624fe58783745f70e80bbd07fe2a588
frindaddy <58454817+frindaddy@users.noreply.github.com>
2023-07-11 18:43:04 -0500

    prelim mobile filter panel styling

--- client/src/components/DrinkList/FilterPanel.js
+++ client/src/components/DrinkList/FilterPanel.js
@@ -41,30 +41,34 @@ const FilterPanel = ({setShowFilterPanel, tagFilterList, setTagFilterList, glass
 
     return (
         <>
-            <div className="filter-category-container">
+            <div className="filter-panel-container">
                 {Object.keys(allTags).map((cat)=>{
-                    return <div className="filter-tag-list">
+                    return <div className="filter-category-container">
                         <p className="filter-category-title">{cat.charAt(0).toUpperCase() + cat.slice(1)}</p>
-                        {allTags[cat].map((tagName)=>{
-                            let selected = tagFilterList.includes(cat+'>'+tagName);
+                        <div className="filter-category-tag-container">
+                            {allTags[cat].map((tagName)=>{
+                                let selected = tagFilterList.includes(cat+'>'+tagName);
+                                return (
+                                    <div className="tag-container">
+                                        <div onClick={()=>{onTagClick(cat, tagName)}} className={'tag clickable unselectable ' + (selected ? '':'unselected-tag-filter')} style={selected ? {backgroundColor: getColor({category: cat, value: tagName})}: {}}>{tagName}</div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                })}
+                <div className="filter-category-container">
+                    <p className="filter-category-title">Glass</p>
+                    <div className="filter-category-tag-container">
+                        {allGlasses.map((glass) => {
+                            let selected = glassFilterList.includes(glass);
                             return (
                                 <div className="tag-container">
-                                    <div onClick={()=>{onTagClick(cat, tagName)}} className={'tag clickable unselectable ' + (selected ? '':'unselected-tag-filter')} style={selected ? {backgroundColor: getColor({category: cat, value: tagName})}: {}}>{tagName}</div>
+                                    <div onClick={()=>{onGlassClick(glass)}} className={'tag clickable unselectable ' + (selected ? '':'unselected-tag-filter')} style={selected ? {backgroundColor: getColor({category: 'glass'})}: {}}>{getDisplayName(glass)}</div>
                                 </div>
                             )
                         })}
                     </div>
-                })}
-                <div>
-                    <p className="filter-category-title">Glass</p>
-                    {allGlasses.map((glass) => {
-                        let selected = glassFilterList.includes(glass);
-                        return (
-                            <div className="tag-container">
-                                <div onClick={()=>{onGlassClick(glass)}} className={'tag clickable unselectable ' + (selected ? '':'unselected-tag-filter')} style={selected ? {backgroundColor: getColor({category: 'glass'})}: {}}>{getDisplayName(glass)}</div>
-                            </div>
-                        )
-                    })}
                 </div>
             </div>
             <div className='filter-chevron'><FaChevronUp style={{cursor:"pointer"}} onClick={() => {setShowFilterPanel(false)}}/></div>
