1eeacdcd5bab77d25045cd48c5d7fb586529056b
sidezbros <ts.sides@gmail.com>
2023-12-27 11:22:11 -0600

    Update TagEntry Categories

---
+++ client/src/definitions/TagCategories.js
@@ -0,0 +1,13 @@
+const TagCategories = [
+    {name: "spirit", localization: "Spirit"},
+    {name: "style", localization: "Style"},
+    {name: "taste", localization: "Taste"},
+    {name: "mix", localization: "Mix"},
+    {name: "color", localization: "Color"},
+    {name: "season", localization: "Season"},
+    {name: "temp", localization: "Temp"},
+    {name: "recommendation", localization: "Recommendation"},
+    {name: "misc", localization: "Misc."}
+];
+
+export default TagCategories;
