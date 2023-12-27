b0ce6d91a245d26c50635025f10c344dc99ff654
sidezbros <ts.sides@gmail.com>
2023-12-27 15:27:30 -0600

    stabilized dot color
    I spent a lot of time experimenting with this. This should stabilize the dot color but fixing it will require large rewrites to the structure of the site

--- client/src/components/DotColor.js
+++ client/src/components/DotColor.js
@@ -8,7 +8,7 @@ const DotColor = ({toggleAdminMode}) => {
         let i = Math.floor(Math.random() * 5);
         let colors = ["#7856a1", "#5096ab", "#d6a038", "#6f8548", "#d66d2a"];
         setColor(colors[i]);
-    }, [color]);
+    }, []);
 
     return (
         <div onClick={toggleAdminMode} style={{WebkitTextStrokeColor: color, marginLeft: "-5px"}}>.</div>
