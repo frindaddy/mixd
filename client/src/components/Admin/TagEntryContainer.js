151ac88adddfd2793a2d95c06ef1e3dc073d1554
sidezbros <20784865+sidezbros@users.noreply.github.com>
2024-09-25 17:32:35 -0500

    Implement Tag Selection Panel

--- client/src/components/Admin/TagEntryContainer.js
+++ client/src/components/Admin/TagEntryContainer.js
@@ -4,28 +4,52 @@ import {FaPlus} from "react-icons/fa";
 import FilterPanel from "../DrinkList/FilterPanel";
 const TagEntryContainer = ({inputs, setInputs}) => {
 
-    const [tags, setTags] = useState([{}]);
+    const [customTags, setCustomTags] = useState([{}]);
     const [tagSelectedList, setTagSelectedList] = useState([]);
+    const [loadLocked, setLoadLocked] = useState(false);
 
     useEffect(() => {
-        if(inputs.tags){
-            setTags(inputs.tags);
+        if(inputs.tags && !loadLocked){
+            setLoadLocked(true);
+            setTagSelectedList(inputs.tags.map((inputTag)=>{
+                return inputTag.category+'>'+inputTag.value
+            }))
         }
     }, [inputs]);
 
-    const updateAllTags = (new_tags) => {
-        setTags(new_tags);
-        setInputs(values => ({...values, tags: new_tags}));
+    const validateAndFormatTags = (selectedTagStrings, customTagStrings) => {
+        let allTags = [...new Set([...selectedTagStrings, ...customTagStrings])]
+        return allTags.map((tagString) => {
+            let split = tagString.split('>')
+            return {category: split[0], value: split[1]}
+        })
+    }
+
+    const updateSelectedTags = (selTags) => {
+        let customTagString = customTags.map((inputTag)=>{
+            return inputTag.category+'>'+inputTag.value
+        })
+        setTagSelectedList(selTags)
+        setInputs(values => ({...values, tags: validateAndFormatTags(selTags, customTagString)}));
+    }
+
+    const updateCustomTags = (new_customTags) => {
+        let customTagString = new_customTags.map((inputTag)=>{
+            return inputTag.category+'>'+inputTag.value
+        })
+        setCustomTags(new_customTags);
+        setInputs(values => ({...values, tags: validateAndFormatTags(tagSelectedList, customTagString)}));
     }
 
     return (
         <div>
-            <FilterPanel setShowFilterPanel={undefined} tagFilterList={tagSelectedList} setTagFilterList={setTagSelectedList} glassFilterList={null} setGlassFilterList={null} tagMenu={true}/>
-            {tags.map((tag, index) => {
-                return <TagEntry index={index} tags={tags} setTags={updateAllTags}/>
+            <FilterPanel setShowFilterPanel={undefined} tagFilterList={tagSelectedList} setTagFilterList={updateSelectedTags} glassFilterList={null} setGlassFilterList={null} tagMenu={true}/>
+            <p>Add new tag:</p>
+            {customTags.map((tag, index) => {
+                return <TagEntry index={index} tags={customTags} setTags={updateCustomTags}/>
             })}
             <div style={{display: "flex", justifyContent: "center", marginTop: "5px"}}>
-                <FaPlus onClick={() => {setTags([...tags, {}])}}/>
+                <FaPlus onClick={() => {setCustomTags([...customTags, {}])}}/>
             </div>
         </div>
     )
