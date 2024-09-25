d5fec68a44dc90a23ab139958e51151812108dc1
sidezbros <20784865+sidezbros@users.noreply.github.com>
2024-09-24 22:10:48 -0500

    Unit input change

--- client/src/components/Admin/IngredientEntry.js
+++ client/src/components/Admin/IngredientEntry.js
@@ -3,15 +3,21 @@ import {FaMinus} from "react-icons/fa";
 
 const IngredientEntry = ({index, ingredients, setIngredients, allIngredients}) => {
     const [ingredient, setIngredient] = useState({});
+    const units = ['oz']
 
     useEffect(() => {
         if(ingredients[index].unit && ingredients[index].ingredient){
-            setIngredient({amount: ingredients[index].amount, unit: ingredients[index].unit, ingredient: ingredients[index].ingredient})
+            let dropdown = ingredients[index].unit
+            if(!units.includes(dropdown) && dropdown !== undefined) dropdown = 'custom'
+            setIngredient({amount: ingredients[index].amount, unit: ingredients[index].unit, ingredient: ingredients[index].ingredient, unit_dropdown: dropdown})
         }
     }, [index, ingredients]);
 
     function handleFormChange(e) {
         let new_ingredient = {...ingredient, [e.target.name]:e.target.value};
+        if(new_ingredient['unit_dropdown'] !== "custom"){
+            new_ingredient['unit'] = new_ingredient['unit_dropdown']
+        }
         let temp_ingredients = ingredients;
         temp_ingredients[index] = new_ingredient;
         setIngredient(new_ingredient);
@@ -21,7 +27,14 @@ const IngredientEntry = ({index, ingredients, setIngredients, allIngredients}) =
     return (
         <div>
             <input type='text' placeholder='2' size='10' onChange={handleFormChange} value={ingredient.amount||""} name='amount'/>
-            <input type='text' placeholder='oz' size='10' onChange={handleFormChange} value={ingredient.unit||""} name='unit'/>
+            <select name='unit_dropdown' onChange={handleFormChange}>
+                <option value='' disabled={true} selected={!ingredient.unit_dropdown}>No Unit</option>
+                {units.map((unit) => {
+                    return <option value={unit} selected={ingredient.unit_dropdown === unit}>{unit}</option>
+                })}
+                <option value='custom' selected={ingredient.unit_dropdown === 'custom'}>Custom</option>
+            </select>
+            {ingredient.unit_dropdown === "custom" && <input type='text' placeholder='oz' size='10' onChange={handleFormChange} value={ingredient.unit||""} name='unit'/>}
             <select name='ingredient' onChange={handleFormChange}>
                 <option value='' disabled={true} selected={!ingredient.ingredient}>Select Ingredient</option>
                 {allIngredients.map((ingredientOption)=>{
