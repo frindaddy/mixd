157329db5ac3ebc81f4583d1e99c78ea5dfc637b
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-02-23 14:56:14 -0600

    Basic router implementation

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,13 @@ import DrinkList from "./components/DrinkList/DrinkList";
 import React, { useState,  useEffect } from "react";
 import DrinkInfo from "./components/DrinkInfo/DrinkInfo";
 import CreateDrink from "./components/Admin/CreateDrink";
-import axios from "axios";
 import ManageIngredients from "./components/Admin/ManageIngredients";
 import ViewIngredients from "./components/Ingredients/ViewIngredients";
-
-let currentYear = new Date().getFullYear();
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import PageFrame from "./pages/PageFrame";
+import Page404 from "./pages/Page404";
 
 function App() {
-    const [appInfo, setAppInfo] = useState({});
     const [currentPage, setCurrentPage] = useState("drinkList");
     const [currentDrink, setCurrentDrink] = useState("");
     const [previousDrinkList, setPreviousDrinkList] = useState([{name:"Loading Drinks..."}]);
@@ -20,29 +19,20 @@ function App() {
     const [ingrFilter, setIngrFilter] = useState(["",""]);
     const [adminKey, setAdminKey] = useState();
 
-    useEffect(() => {
-        axios.get('api/app-info')
-            .then((res) => {
-                if (res.data) {
-                    setAppInfo(res.data);
-                }
-            }).catch((err) => console.log(err));
-    }, []);
-
     return (
-    <div className="App">
-        {currentPage === "drinkList" && <DrinkList setCurrentPage={setCurrentPage} setCurrentDrink={setCurrentDrink} searchText={searchText} setSearchText={setSearchText} adminKey={adminKey} setAdminKey={setAdminKey} previousDrinkList={previousDrinkList} setPreviousDrinkList={setPreviousDrinkList} ingrFilter={ingrFilter} setIngrFilter={setIngrFilter}/>}
-        {currentPage === "drinkInfo" && <DrinkInfo drinkID={currentDrink} setCurrentPage={setCurrentPage} setCurrentDrink={setCurrentDrink}/>}
-        {currentPage === "createDrink" && <CreateDrink setCurrentPage={setCurrentPage} drinkID={null} adminKey={adminKey}/>}
-        {currentPage === "updateDrink" && <CreateDrink setCurrentPage={setCurrentPage} drinkID={currentDrink} adminKey={adminKey}/>}
-        {currentPage === "manageIngredients" && <ManageIngredients setCurrentPage={setCurrentPage} adminKey={adminKey}/>}
-        {currentPage === "viewIngredients" && <ViewIngredients setCurrentPage={setCurrentPage} setIngrFilter={setIngrFilter}/>}
-        <footer>
-            <p>©{currentYear} by Jacob Thweatt and Trevor Sides. All Rights Reserved.<br/>
-                Powered by our pure genius.<br/>
-                v{appInfo.version}</p>
-        </footer>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<PageFrame />}>
+                <Route index element={<DrinkList setCurrentPage={setCurrentPage} setCurrentDrink={setCurrentDrink} searchText={searchText} setSearchText={setSearchText} adminKey={adminKey} setAdminKey={setAdminKey} previousDrinkList={previousDrinkList} setPreviousDrinkList={setPreviousDrinkList} ingrFilter={ingrFilter} setIngrFilter={setIngrFilter}/>}/>
+                <Route path="drink" element={<DrinkInfo drinkID={currentDrink} setCurrentPage={setCurrentPage} setCurrentDrink={setCurrentDrink}/>}/>
+                <Route path="create_drink" element={<CreateDrink setCurrentPage={setCurrentPage} drinkID={null} adminKey={adminKey}/>}/>
+                <Route path="update_drink" element={<CreateDrink setCurrentPage={setCurrentPage} drinkID={currentDrink} adminKey={adminKey}/>}/>
+                <Route path="manage_ingredients" element={<ManageIngredients setCurrentPage={setCurrentPage} adminKey={adminKey}/>}/>
+                <Route path="view_ingredients" element={<ViewIngredients setCurrentPage={setCurrentPage} setIngrFilter={setIngrFilter}/>}/>
+                <Route path="*" element={<Page404 />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
     );
 }
 
