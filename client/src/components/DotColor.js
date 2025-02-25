8ba2f4c0157e8f25c7d3f314c46ce7984eca5865
frindaddy <58454817+frindaddy@users.noreply.github.com>
2025-02-25 16:13:39 -0500

    cycle dot color using cookies

--- client/src/components/DotColor.js
+++ client/src/components/DotColor.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
 
 const DotColor = ({toggleAdminMode}) => {
 
     const [color, setColor] = useState('#FFFFFF');
+    const [cookies, setCookie] = useCookies(['colorIndex']);
 
     useEffect(() => {
-        let i = Math.floor(Math.random() * 5);
-        let colors = ["#7856a1", "#5096ab", "#d6a038", "#6f8548", "#d66d2a"];
-        setColor(colors[i]);
+        let dotColors = ["#7856a1", "#5096ab", "#d6a038", "#6f8548", "#d66d2a"];
+        let colorIndex = cookies.colorIndex;
+        setColor(dotColors[colorIndex])
+        if(colorIndex < 4) {
+            setCookie('colorIndex', ++colorIndex)
+        } else {
+            setCookie('colorIndex', 0);
+        }
     }, []);
 
     return (
