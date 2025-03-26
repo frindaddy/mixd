790efc32722ab456d2cd7364dc89e0061b454ad4
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-03-25 21:36:16 -0500

    Move login panel out of the way

--- client/src/pages/ViewIngredients.js
+++ client/src/pages/ViewIngredients.js
@@ -91,35 +91,6 @@ const ViewIngredients = ({setIngrFilter, setUserDrinksReq, user, setUser}) => {
 
     return (
         <div>
-            <div>
-                <span>{"User ID:"+(user === null ? '':' '+user)}</span>
-                {user !== null && <FaTrash style={{cursor:'pointer', marginLeft:'10px'}} onClick={clear_user_id}/>}
-            </div>
-            {user !== null && <div>
-                <FaEdit className="sorted-filter-icon" style={{backgroundColor: editUserIngr? "3B3D3F":"", cursor:'pointer'}} onClick={()=>{setEditUserIngr(!editUserIngr)}}/>
-                <div>
-                    <span>Allowed Missing Ingredients:  </span>
-                    <select onChange={(e)=> setSearchSettings({...searchSettings, tol: e.target.value})}>
-                        {[0,1,2,3,4,5].map(index=> {
-                            return <option value={index} selected={searchSettings.tol === index}>{index}</option>;
-                        })}
-                    </select>
-                </div>
-                <div>
-                    <span>Ignore NA Ingredients:  </span>
-                    <input type='checkbox' checked={searchSettings.no_na} onChange={(e)=> setSearchSettings({...searchSettings, no_na: e.target.checked})}></input>
-                </div>
-                <div>
-                    <span>Exact Search:  </span>
-                    <input type='checkbox' checked={searchSettings.strict} onChange={(e)=> setSearchSettings({...searchSettings, strict: e.target.checked})}></input>
-                </div>
-                <FaSearch style={{cursor:'pointer', marginTop:'15px'}} onClick={search_user_drinks}/>
-            </div>}
-            {user === null && <div>
-                <input inputMode='numeric' content='text' placeholder='00000' value={userField||''} onChange={(e)=> setUserField(parseInt(e.target.value.substring(0,5)) || null)} onKeyDownCapture={(e)=>{checkEnter(e)}}></input>
-                <FaCheck style={{marginLeft: '10px', cursor:'pointer'}} onClick={get_user_ingredients} />
-            </div>}
-
             <div style={{display: "flex", justifyContent: "center", alignItems:"center", marginLeft:"-53px"}}>
                 <FaSortAmountDown className="sorted-filter-icon" style={{backgroundColor: sorted? "3B3D3F":""}} onClick={()=>{setSorted(!sorted)}}/>
                 <h1 className="all-ingredients-title">All Ingredients</h1>
@@ -131,6 +102,39 @@ const ViewIngredients = ({setIngrFilter, setUserDrinksReq, user, setUser}) => {
                     </div>
                 </div>;
             })}
+            <br />
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <div>
+                    <div>
+                        <span>{"User ID:"+(user === null ? '':' '+user)}</span>
+                        {user !== null && <FaTrash style={{cursor:'pointer', marginLeft:'10px'}} onClick={clear_user_id}/>}
+                    </div>
+                    {user !== null && <div>
+                        <FaEdit className="sorted-filter-icon" style={{backgroundColor: editUserIngr? "3B3D3F":"", cursor:'pointer'}} onClick={()=>{setEditUserIngr(!editUserIngr)}}/>
+                        <div>
+                            <span>Allowed Missing Ingredients:  </span>
+                            <select onChange={(e)=> setSearchSettings({...searchSettings, tol: e.target.value})}>
+                                {[0,1,2,3,4,5].map(index=> {
+                                    return <option value={index} selected={searchSettings.tol === index}>{index}</option>;
+                                })}
+                            </select>
+                        </div>
+                        <div>
+                            <span>Ignore NA Ingredients:  </span>
+                            <input type='checkbox' checked={searchSettings.no_na} onChange={(e)=> setSearchSettings({...searchSettings, no_na: e.target.checked})}></input>
+                        </div>
+                        <div>
+                            <span>Exact Search:  </span>
+                            <input type='checkbox' checked={searchSettings.strict} onChange={(e)=> setSearchSettings({...searchSettings, strict: e.target.checked})}></input>
+                        </div>
+                        <FaSearch style={{cursor:'pointer', marginTop:'15px'}} onClick={search_user_drinks}/>
+                    </div>}
+                    {user === null && <div>
+                        <input inputMode='numeric' content='text' placeholder='00000' value={userField||''} onChange={(e)=> setUserField(parseInt(e.target.value.substring(0,5)) || null)} onKeyDownCapture={(e)=>{checkEnter(e)}}></input>
+                        <FaCheck style={{marginLeft: '10px', cursor:'pointer'}} onClick={get_user_ingredients} />
+                    </div>}
+                </div>
+            </div>
         </div>
     )
 }
