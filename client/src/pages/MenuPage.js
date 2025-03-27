8c777f1e1b197dd222cb6e55c9ec8bc5b53c7a98
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-03-27 10:22:09 -0500

    display menu drink array

--- client/src/pages/MenuPage.js
+++ client/src/pages/MenuPage.js
@@ -1,17 +1,19 @@
 import {useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
-const MenuPage = () => {
+import DrinkArray from "../components/DrinkList/DrinkArray";
+const MenuPage = ({setShowLoader}) => {
 
     const { menu_id } = useParams();
     const navigate = useNavigate();
 
-    const [menu, setMenu] = useState({});
+    const [menu, setMenu] = useState({drinkList:[{name:"Loading Drinks..."}]});
 
     useEffect(() => {
         axios.get('/api/menu/'+menu_id)
             .then((res) => {
                 if (res.data) {
+                    console.log(res.data.drinkList)
                     setMenu(res.data)
                 } else {
                     navigate('/404', {replace: true});
@@ -25,6 +27,7 @@ const MenuPage = () => {
         <div>
             <h1>Menu</h1>
             <p>{'ID: '+menu.menu_id}</p>
+            <DrinkArray drinkList={menu.drinkList} filter={{text: "", tags: [], glasses: []}} setShowLoader={setShowLoader}/>
         </div>
     )
 };
