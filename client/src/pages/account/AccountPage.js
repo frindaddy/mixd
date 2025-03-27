29f4fe94ba8cf560c0821a1330ff2a63591396f8
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-03-27 14:21:15 -0500

    create account nav

--- client/src/pages/account/AccountPage.js
+++ client/src/pages/account/AccountPage.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {FaSignOutAlt, FaStar} from "react-icons/fa";
+import "../../format/Account.css";
 
 const AccountPage = ({user, setUser}) => {
 
@@ -25,6 +26,16 @@ const AccountPage = ({user, setUser}) => {
         <div>
             <FaSignOutAlt className='user_icon' onClick={logout} />
             <h1>{'Account #'+user.user_id}{user.adminKey && <FaStar style={{color:'gold', marginLeft: '10px', marginBottom:'-3px'}} title='User is an admin'/>}</h1>
+            <div className='account-nav'>
+                <span onClick={()=>setCurrentTab('myBar')}>My Bar</span>
+                <span onClick={()=>setCurrentTab('settings')}>Account Settings</span>
+                {user.adminKey && <span onClick={()=>setCurrentTab('admin')}>Admin Controls</span>}
+            </div>
+            <div>
+                {currentTab === 'myBar' && <p>My Bar</p>}
+                {currentTab === 'settings' && <p>Account Settings</p>}
+                {currentTab === 'admin' && <p>Admin Controls</p>}
+            </div>
         </div>
     )
 };
