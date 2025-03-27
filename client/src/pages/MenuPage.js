ed269b50038e8f088203c2b36d58c4b1ea93a9d3
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-03-27 10:01:50 -0500

    fetch menu

--- client/src/pages/MenuPage.js
+++ client/src/pages/MenuPage.js
@@ -1,12 +1,30 @@
-import {useLocation, useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 const MenuPage = () => {
 
     const { menu_id } = useParams();
+    const navigate = useNavigate();
+
+    const [menu, setMenu] = useState({});
+
+    useEffect(() => {
+        axios.get('/api/menu/'+menu_id)
+            .then((res) => {
+                if (res.data) {
+                    setMenu(res.data)
+                } else {
+                    navigate('/404', {replace: true});
+                }
+            }).catch(() => {
+            navigate('/404', {replace: true});
+        });
+    }, [menu_id]);
 
     return (
         <div>
             <h1>Menu</h1>
-            <p>{'ID: '+menu_id}</p>
+            <p>{'ID: '+menu.menu_id}</p>
         </div>
     )
 };
