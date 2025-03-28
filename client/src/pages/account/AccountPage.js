0f30976a7004da9b3145cc9ebca046aa5e02cdf0
frindaddy <58454817+frindaddy@users.noreply.github.com>
2025-03-28 12:38:46 -0400

    init account page styling

--- client/src/pages/account/AccountPage.js
+++ client/src/pages/account/AccountPage.js
@@ -27,13 +27,16 @@ const AccountPage = ({user, setUser, setIngrFilter, setUserDrinksReq }) => {
     }
 
     return (
-        <div>
+        <div style={{paddingLeft:"2%"}}>
             <FaSignOutAlt className='user_icon' onClick={logout} />
-            <h1>{(user.username ? user.username:'Account')+' #'+user.user_id}{user.adminKey && <FaStar style={{color:'gold', marginLeft: '10px', marginBottom:'-3px'}} title='User is an admin'/>}</h1>
+            <div className="account-name">{(user.username ? user.username:'Account')+' #'+user.user_id}{user.adminKey && <FaStar style={{color:'gold', marginLeft: '10px', marginBottom:'-3px'}} title='User is an admin'/>}</div>
             <div className='account-nav'>
                 <span onClick={()=>setCurrentTab('myBar')}>My Bar</span>
+                <div className="account-nav-break"></div>
                 <span onClick={()=>setCurrentTab('menus')}>Menus</span>
+                <div className="account-nav-break"></div>
                 <span onClick={()=>setCurrentTab('settings')}>Account Settings</span>
+                {user.adminKey && <div className="account-nav-break"></div>}
                 {user.adminKey && <span onClick={()=>setCurrentTab('admin')}>Admin Controls</span>}
             </div>
             <div>
