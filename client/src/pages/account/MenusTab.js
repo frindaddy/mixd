343051c34d450b55c3cbabeac2a901c224544285
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-03-28 21:38:03 -0500

    rename menus

--- client/src/pages/account/MenusTab.js
+++ client/src/pages/account/MenusTab.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {FaCheck, FaEdit, FaPlus, FaTrash} from "react-icons/fa";
 import {useNavigate} from "react-router-dom";
+import {FaX} from "react-icons/fa6";
 
 const MenusTab = ({user}) => {
 
@@ -9,6 +10,7 @@ const MenusTab = ({user}) => {
     const [drinkList, setDrinkList] = useState([]);
     const [creatingMenu, setCreatingMenu] = useState(false);
     const [newMenuName, setNewMenuName] = useState('');
+    const [renameIndex, setRenameIndex] = useState(-1);
 
     const navigate = useNavigate();
 
@@ -40,7 +42,7 @@ const MenusTab = ({user}) => {
     }
 
     function startDrinkCreation(){
-        if(!creatingMenu){
+        if(!creatingMenu && renameIndex < 0){
             setCreatingMenu(true);
         }
     }
@@ -50,19 +52,54 @@ const MenusTab = ({user}) => {
             if(res.data){
                 let new_menus= [...menus, {menu_id: res.data.menu_id, name:newMenuName, drinks:[]}];
                 setMenus(new_menus);
+                setNewMenuName('');
                 setCreatingMenu(false);
             }
         })
     }
 
+    function startRename(current_name, menu_index) {
+        if(!creatingMenu){
+            setNewMenuName(current_name || '');
+            setRenameIndex(menu_index);
+        }
+    }
+
+    function cancelRename() {
+        setRenameIndex(-1);
+        setNewMenuName('');
+    }
+
+    function renameMenu(menu_id, menu_index) {
+        if(newMenuName !== '' && menu_id){
+            axios.post('/api/modify_menu', {menu_id: menu_id, name: newMenuName}).then((res)=>{
+                console.log(res);
+                let new_menus = [...menus];
+                new_menus[menu_index].name = newMenuName;
+                setMenus(new_menus);
+                setRenameIndex(-1);
+                setNewMenuName('');
+            });
+        } else {
+            cancelRename();
+        }
+    }
+
     return (
         <div>
             <div>
-                {menus.map(menu => {
+                {menus.map((menu, menu_index) => {
                     return <div style={{width: '200px', height: '200px', float:'left', padding:'10px', margin: '10px', backgroundColor:"gray", borderRadius: '10px', cursor:'pointer'}}>
-                        <span onClick={()=>navigate('/menu/'+menu.menu_id)}>{menu.name || "Menu " + menu.menu_id}</span>
-                        <FaTrash style={{float: 'right'}} onClick={()=>confirmDeleteMenu(menu.menu_id, menu.name)}/>
-                        <FaEdit style={{float: 'right', marginRight:'10px'}} />
+                        {renameIndex >= 0 && renameIndex === menu_index && <div>
+                            <input style={{width:'140px', border: 'none', outline:'none', fontSize: '16px', backgroundColor: 'darkgray'}} name='rename' value={newMenuName} onChange={(e)=>setNewMenuName(e.target.value)}/>
+                            <FaCheck style={{float:'right'}} onClick={()=>{renameMenu(menu.menu_id, menu_index)}}/>
+                            <FaX style={{marginRight: '10px', float:'right'}} onClick={cancelRename}/>
+                        </div>}
+                        {renameIndex !== menu_index && <div>
+                            <span onClick={()=>navigate('/menu/'+menu.menu_id)}>{menu.name || "Menu " + menu.menu_id}</span>
+                            <FaTrash style={{float: 'right'}} onClick={()=>confirmDeleteMenu(menu.menu_id, menu.name)}/>
+                            <FaEdit style={{float: 'right', marginRight:'10px'}} onClick={()=> startRename(menu.name, menu_index)}/>
+                        </div>}
                         <div onClick={()=>navigate('/menu/'+menu.menu_id)}>
                             {menu.drinks.map(drink => {
                                 let filtered_drinks = drinkList.filter(d=>d.uuid == drink)
