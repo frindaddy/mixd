1d37eb0398d735a1ac18522c31094767552a7f88
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-03-30 21:14:10 -0500

    Account Dropdown

--- client/src/components/AccountShortcut.js
+++ client/src/components/AccountShortcut.js
@@ -1,24 +1,38 @@
-import React from "react"
+import React, {useState} from "react"
 import {FaRegUserCircle, FaUserCircle} from "react-icons/fa";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const AccountShortcut = ({user}) => {
+
+    const [showDropdown, setShowDropdown] = useState(false);
 
     const navigate = useNavigate();
 
     function userIconClicked() {
-        navigate('/account/login');
+        if(user.user_id){
+            setShowDropdown(!showDropdown);
+        } else {
+            navigate('/account/login');
+        }
     }
 
     return (
-        <>
+        <div className='account-shortcut'>
             <div onClick={userIconClicked} style={{cursor:'pointer'}}>
-                {user.username && <p style={{position:'absolute', right: '45px', top:'-2px'}}>{user.username}</p>}
+                {user.username && <span style={{position:'absolute', right: '40px', top:'4px'}}>{user.username}</span>}
                 {!user.user_id && <FaRegUserCircle className="user_icon" />}
                 {user.user_id && <FaUserCircle className="user_icon" />}
-            </div>
-        </>
 
+            </div>
+            <div className='dropdown-triangle' />
+            {showDropdown && <div className='account-dropdown'>
+                <Link to='/account'>My Account</Link>
+                <hr />
+                <Link to='/account/bar'>My Bar</Link>
+                <hr />
+                <Link to='/account/menu'>My Menus</Link>
+            </div>}
+        </div>
     )
 }
 
