63a9f8f061734d63da8e83823fc3025b20aa5df0
frindaddy <58454817+frindaddy@users.noreply.github.com>
2025-04-02 19:33:38 -0400

    fix ingredient use count grammar

--- client/src/components/Ingredients/IngredientListEntry.js
+++ client/src/components/Ingredients/IngredientListEntry.js
@@ -5,7 +5,7 @@ const IngredientListEntry = ({ingredient, onIngredientClick}) => {
     return (
         <>
             <p className="ingredient-entry" onClick={()=>{onIngredientClick(ingredient)}} style={{cursor: "pointer"}}>{ingredient.name}</p>
-            <p className="ingredient-use-count">({ingredient.count} uses)</p>
+            <p className="ingredient-use-count">({ingredient.count} {ingredient.count > 1 ? "uses":"use"})</p>
         </>
     )
 }
