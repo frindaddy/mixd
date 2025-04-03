a8d740b647f913c0e98750a25ab913ddc86d03c5
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-04-02 19:11:57 -0500

    add ingredients to filter panel

--- client/src/components/DrinkList/FilterPanel.js
+++ client/src/components/DrinkList/FilterPanel.js
@@ -4,17 +4,26 @@ import {getColor} from "../DrinkTags";
 import {FaChevronUp} from "react-icons/fa";
 import TagCategories from "../../definitions/TagCategories";
 import "../../format/FilterPanel.css";
+import IngredientCategories from "../../definitions/IngredientCategories";
 
 const FilterPanel = ({toggleFilterPanel, searchParams, updateSearchParams}) => {
 
     const [allTags, setAllTags] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
     const [localSelectedTags, setLocalSelectedTags] = useState([]);
+    const [localIngredient, setLocalIngredient] = useState('');
 
     useEffect(() => {
         axios.get('/api/tags/')
             .then((res) => {
                 if (res.data) {
                     setAllTags(res.data);
+                }
+            }).catch((err) => console.log(err));
+        axios.get('/api/get_ingredients')
+            .then((res) => {
+                if (res.data) {
+                    setIngredients(res.data);
                 }
             }).catch((err) => console.log(err));
     }, []);
@@ -40,6 +49,16 @@ const FilterPanel = ({toggleFilterPanel, searchParams, updateSearchParams}) => {
         setLocalSelectedTags(newTagList);
     }
 
+    function onIngrClick(ingr_uuid){
+        if(searchParams.ingredient === ingr_uuid){
+            updateSearchParams('ingredient', undefined);
+            setLocalIngredient('');
+        } else {
+            updateSearchParams('ingredient', ingr_uuid);
+            setLocalIngredient(ingr_uuid);
+        }
+    }
+
     return (
         <>
             <div className="filter-panel-container">
@@ -54,6 +73,24 @@ const FilterPanel = ({toggleFilterPanel, searchParams, updateSearchParams}) => {
                                         <div onClick={()=>{onTagClick(cat.name, tag.value)}}
                                              className={'tag clickable unselectable ' + (selected ? '':'unselected-tag-filter')}
                                              style={selected ? {backgroundColor: getColor({category: cat.name, value: tag.value})}: {}}>{tag.value}</div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                })}
+                <br />
+                {IngredientCategories.map((cat)=>{
+                    return <div className="filter-category-container">
+                        <p className="filter-category-title">{cat.header}</p>
+                        <div className="filter-category-tag-container">
+                            {ingredients.filter(ingr=>ingr.category === cat.name).map((ingr)=>{
+                                let selected = searchParams.ingredient === ingr.uuid;
+                                return (
+                                    <div className="tag-container">
+                                        <div onClick={()=>{onIngrClick(ingr.uuid)}}
+                                             className={'tag clickable unselectable ' + (selected ? '':'unselected-tag-filter')}
+                                             style={selected ? {backgroundColor: 'green'}: {}}>{ingr.name}</div>
                                     </div>
                                 )
                             })}
