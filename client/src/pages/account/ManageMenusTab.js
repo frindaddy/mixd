62d0332a2042c749d1613fec0e176df22a8d686b
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-04-03 09:22:55 -0500

    add force delete button

--- client/src/pages/account/ManageMenusTab.js
+++ client/src/pages/account/ManageMenusTab.js
@@ -26,6 +26,19 @@ const ManageMenusTab = ({adminKey, user}) => {
         }).catch((err) => console.log(err));
     }
 
+    function confirmDeleteMenu(menuToDelete) {
+        if(window.confirm('Are you sure you want to force-delete menu "'+(menuToDelete.name || menuToDelete.menu_id) + '" by ' + getUsername(menuToDelete.users) + '?') === true){
+            axios.delete('/api/menu_forced/'+menuToDelete.menu_id, {headers:{Authorization: `Bearer ${adminKey}`}})
+                .then(() => {
+                    setMenus(menus.filter(menu => menu.menu_id !== menuToDelete.menu_id));
+                }).catch((err) => {
+                setErrorMsg('Failed to delete menu. Internal server error '+err.response.status);
+            });
+        } else {
+            alert('Menu not deleted.');
+        }
+    }
+
     function setFeatured(menu_id, remove) {
         axios.post('/api/feature_menu', {menu_id: menu_id, remove: remove}, {
                 headers: {
@@ -66,6 +79,7 @@ const ManageMenusTab = ({adminKey, user}) => {
                         <span style={{fontSize: '14px', marginLeft: '5px'}} className="manage-ingredients-entry">{getUsername(menuEntry.users)}</span>
                         {menuEntry.featured && <FaStar style={{cursor:'pointer', marginLeft:'8px'}} onClick={()=>{setFeatured(menuEntry.menu_id, true)}}/>}
                         {!menuEntry.featured && <FaRegStar style={{cursor:'pointer', marginLeft:'8px'}} onClick={()=>{setFeatured(menuEntry.menu_id, false)}}/>}
+                        <FaTrash style={{marginLeft:'10px', cursor:'pointer'}} onClick={()=>{confirmDeleteMenu(menuEntry)}}/>
                     </div>
                 </div>
             })}
