556519330a846a8f2e0f6394130c164039c14bd5
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-04-04 13:28:46 -0500

    adminKey -> token

--- client/src/App.js
+++ client/src/App.js
@@ -33,17 +33,17 @@ function App() {
             <Route path="/" element={<Layout showLoader={showLoader} setShowLoader={setShowLoader} user={user} setUser={setUser}/>}>
                 <Route index element={<DrinkList setShowLoader={setShowLoader} searchText={searchText} setSearchText={setSearchText} user={user} setUser={setUser} searchIngredient={searchIngredient} setSearchIngredient={setSearchIngredient} searchTags={searchTags} setSearchTags={setSearchTags} myBarSearch={myBarSearch} setMyBarSearch={setMyBarSearch}/>}/>
                 <Route path=":drink_identifier" element={<DrinkInfo setShowLoader={setShowLoader}/>}/>
-                <Route path="create_drink" element={<CreateDrink adminKey={user.adminKey}/>}/>
-                <Route path="update_drink/:uuid" element={<CreateDrink adminKey={user.adminKey}/>}/>
+                <Route path="create_drink" element={<CreateDrink adminKey={user.token}/>}/>
+                <Route path="update_drink/:uuid" element={<CreateDrink adminKey={user.token}/>}/>
                 <Route path="account" element={<AccountPage user={user}/>} >
                     <Route index element={<MyAccountTab user={user} setUser={setUser}/>}></Route>
                     <Route path="bar" element={<MyBarTab setMyBarSearch={setMyBarSearch} user={user} />}></Route>
                     <Route path="menus" element={<MenusTab user={user}/>}></Route>
                     <Route path="statistics" element={<StatisticsTab setSearchIngredient={setSearchIngredient}/>}></Route>
-                    <Route path="edit_ingredients" element={<ManageIngredientsTab adminKey={user.adminKey} />}></Route>
+                    <Route path="edit_ingredients" element={<ManageIngredientsTab adminKey={user.token} />}></Route>
                     <Route path="edit_drinks" element={<ManageDrinksTab user={user} setShowLoader={setShowLoader}/>}></Route>
-                    <Route path="edit_menus" element={<ManageMenusTab adminKey={user.adminKey} user={user}/>}></Route>
-                    <Route path="users" element={<ManageUsersTab adminKey={user.adminKey} user={user}/>}></Route>
+                    <Route path="edit_menus" element={<ManageMenusTab adminKey={user.token} user={user}/>}></Route>
+                    <Route path="users" element={<ManageUsersTab adminKey={user.token} user={user}/>}></Route>
                 </Route>
                 <Route path="account/login" element={<LoginPage user={user} setUser={setUser} />}/>
                 <Route path="menu/:menu_id" element={<MenuPage setShowLoader={setShowLoader}/>}/>
