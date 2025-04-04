2c148e45790b32066433cfe894d33c05c0704d12
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-04-03 19:50:15 -0500

    fix search box on manage drinks

--- client/src/components/DrinkList/DrinkArray.js
+++ client/src/components/DrinkList/DrinkArray.js
@@ -1,13 +1,24 @@
 import DrinkEntry from "./DrinkEntry";
+import {useEffect, useState} from "react";
 
-const DrinkArray = ({ drinkList, adminKey, getDrinkList, setShowLoader, menuSettings, editMenu, showMenuDesc}) => {
+const DrinkArray = ({ drinkList, adminKey, getDrinkList, setShowLoader, menuSettings, editMenu, showMenuDesc, filterText}) => {
+
+    const [filteredDrinkList, setFilteredDrinkList] = useState([]);
+
+    useEffect(()=>{
+        if(filterText) {
+            setFilteredDrinkList(drinkList.filter((drink => {
+                return drink.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
+            })));
+        }
+    }, [filterText, drinkList])
 
     return (
         <>
-            {drinkList.map((drink) => {
+            {(filterText ? filteredDrinkList : drinkList).map((drink) => {
                 return <DrinkEntry drink={drink} getDrinkList={getDrinkList} adminKey={adminKey} setShowLoader={setShowLoader} menuSettings={menuSettings} editMenu={editMenu} showMenuDesc={showMenuDesc}/>
             })}
-            {drinkList.length === 0 && <p className="filter-match-title">No Drinks Match Your Search</p>}
+            {(filterText ? filteredDrinkList : drinkList).length === 0 && <p className="filter-match-title">No Drinks Match Your Search</p>}
         </>
     )
 }
