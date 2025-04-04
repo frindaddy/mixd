e4f042f4371992d5ab3e9b2430758f610adf9de8
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-04-04 10:17:01 -0500

    clean up import

--- client/src/pages/account/LoginPage.js
+++ client/src/pages/account/LoginPage.js
@@ -1,4 +1,3 @@
-import {FaCheck, FaSignOutAlt} from "react-icons/fa";
 import React, {useEffect, useRef, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
