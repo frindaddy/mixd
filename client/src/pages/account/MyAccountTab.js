a52474e46bf1421027c9ac6c72500d27db329ceb
frindaddy <58454817+frindaddy@users.noreply.github.com>
2025-04-05 20:01:41 -0400

    style myaccount inputs

--- client/src/pages/account/MyAccountTab.js
+++ client/src/pages/account/MyAccountTab.js
@@ -73,13 +73,13 @@ const MyAccountTab = ({user, setUser, removeCookie}) => {
             <div className="account-name">{(user.username ? user.username:'Account')+' #'+user.user_id}{user.isAdmin && <FaStar style={{color:'gold', marginLeft: '10px', marginBottom:'-3px'}} title='User is an admin'/>}</div>
             <p style={{textAlign:"center", fontWeight:"300"}}>Change Username:</p>
             <div style={{display:"flex", justifyContent:"center", marginLeft:"16px"}}>
-                <input name='username' type='text' placeholder={user.username||'Username'} onChange={updateUsernameField} value={usernameField}/>
+                <input className="account-input" name='username' type='text' placeholder={user.username||'Username'} onChange={updateUsernameField} value={usernameField}/>
                 <FaCheck style={{cursor:'pointer', marginLeft: '10px', paddingTop:"2px"}} onClick={submitUsername}/>
             </div>
             {usernameField.length > 0 && <p style={{textAlign:"center", fontWeight:"300"}}>{usernameResponse()}</p>}
             <p style={{textAlign:"center", fontWeight:"300"}}>Change PIN:</p>
             <div style={{display:"flex", justifyContent:"center", marginLeft:"16px"}}>
-                <input name='pin' type='numeric' placeholder='New PIN' onChange={(e)=>{setPinField(parseInt(e.target.value.substring(0,6)) || null)}} value={pinField || ''}/>
+                <input className="account-input" name='pin' type='numeric' placeholder='New PIN' onChange={(e)=>{setPinField(parseInt(e.target.value.substring(0,6)) || null)}} value={pinField || ''}/>
                 <FaCheck style={{cursor:'pointer', marginLeft: '10px', paddingTop:"2px"}} onClick={changePin}/>
             </div>
             <p style={{textAlign:"center", fontWeight:"300", color:"#c0392b"}}>PINs are stored without encryption! Do not use a sensitive PIN!</p>
