77dd7067b765ab549ba494a981117af536917ba6
frindaddy <58454817+frindaddy@users.noreply.github.com>
2025-04-05 21:05:25 -0400

    move menu components to new folder

--- client/src/pages/account/MyMenusTab.js
+++ client/src/pages/account/MyMenusTab.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {FaCheck, FaPlus} from "react-icons/fa";
 import {FaX} from "react-icons/fa6";
-import MenuCard from "../../components/Admin/MenuCard";
+import MenuCard from "../../components/Menus/MenuCard";
 import "../../format/MenuCard.css"
 import "../../format/Tabs.css"
 
