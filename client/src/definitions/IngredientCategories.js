cdbe75e8f6f42b921c2c371b661d34b50b0b34b3
sidezbros <20784865+sidezbros@users.noreply.github.com>
2025-04-07 19:22:26 -0500

    add wine category

--- client/src/definitions/IngredientCategories.js
+++ client/src/definitions/IngredientCategories.js
@@ -4,6 +4,7 @@ const IngredientCategories = [
     {name: "bitters", localization: "Bitters", header: "Bitters"},
     {name: "mixer", localization: "Mixer", header: "Mixers"},
     {name: "syrup", localization: "Syrup", header: "Syrups"},
+    {name: "wine", localization: "Wine", header: "Wines"},
     {name: "misc", localization: "Misc.", header: "Misc."}
 ];
 
